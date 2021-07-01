04bc0d8e13ff4892fe4dd16648c4ccd81bc54d9c
Nguuyenvanvi <86761111+Nguuyenvanvi@users.noreply.github.com>
2021-07-01 12:39:48 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
     introTitle: 'em à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào link này lẻ loi
-    Đi đâu đây`,
-    btnIntro: 'hihi',
+    introDesc: `em có bk a vĩ vừa đẹp trai vừa khoai tođến chừng nào ko `,
+    btnIntro: 'hihi ngại quá',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'iu nhìu nhìu nhìu <33',
-    btnNo: 'Không, Anh trai à :3',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy củ khoai đó ',
+    btnYes: 'iu nhìu nhìu nhìu iu khoai<33',
+    btnNo: 'Không, thích chuối cơ:3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu a vĩ<3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'anh vĩ quá đẹp trai nhức nách lun ',
+    reply: 'anh vĩ quá đẹp trai khoai lại to ',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    messDesc: 'Tối nay 7h anh vĩ dẫn đi nhà nghỉ.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
